9d863ccc42c4201b4c8d6ff85a86d5c6ac29e732
KyawLin1 <didikyawlinn@gmail.com>
2019-06-27 19:50:49 +0800

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# Build images for production
+docker build -t kyawlin/complex-client:latest -t kyawlin/complex-client:$GIT_SHA ./client
+docker build -t kyawlin/complex-server:latest -t kyawlin/complex-server:$GIT_SHA ./server
+docker build -t kyawlin/complex-worker:latest -t kyawlin/complex-worker:$GIT_SHA ./worker
+# Push images to Docker with tag:latest
+docker push kyawlin/complex-client:latest
+docker push kyawlin/complex-server:latest 
+docker push kyawlin/comlex-worker:latest
+# Push images to Docker with tag:$GIT_SHA
+docker push kyawlin/complex-client:$GIT_SHA
+docker push kyawlin/complex-server:$GIT_SHA
+docker push kyawlin/comlex-worker:$GIT_SHA
+kubectl apply -f ./k8s
+kubectl set image deployments/client-deployment client=kyawlin/complex-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=kyawlin/complex-server:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=kyawlin/complex-worker:$GIT_SHA
